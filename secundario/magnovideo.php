b0464ac0c350de124f7eb668628857cf1dd5e508
Andres Leone <forestrf@gmail.com>
2014-04-02 01:21:10 +0200

    Magnovideo comentarios nuevos

--- secundario/magnovideo.php
+++ secundario/magnovideo.php
@@ -1,4 +1,26 @@
 <?php
+
+/*
+http://www.magnovideo.com/?v=BMCX9KSL
+http://www.magnovideo.com/player_config.php?mdid=BMCX9KSL
+
+DESCARGA:
+
+
+
+http://o1.magnovideo.com:8080/storage/files/part1/0/15/111/175/1.mp4?burst=3000k&u=600k&md=1bVi10TyubRRLgKy3UsEjA&e=1393897743
+
+
+<tile_thumbs>http://o1.magnovideo.com:8080//storage/files/part1/0/15/111/175/tmpsmall/tiles.jpg</tile_thumbs>
+
+<video_name>1.mp4</video_name>
+<storage_path>http://o1.magnovideo.com:8080/</storage_path>
+<sto>md=xqBNmNJjsMILgDz7p3J7IQ&e=1393897903</sto>
+
+http://o1.magnovideo.com:8080/storage/files/part1/0/15/111/175/1.mp4?md=xqBNmNJjsMILgDz7p3J7IQ&e=1393897903
+*/
+
+
 function magnovideo(){
 	global $web,$web_descargada;
 	
