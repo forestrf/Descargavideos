712792b06d1054759531f151adf4eb10f25768b2
Andrés Leone <forestrf@gmail.com>
2014-04-06 02:19:35 +0200

    Mejora para imprimir menos código en las búsquedas de audio

--- secundario/plantillas/febrero-res-mp3/resultado_url.php
+++ secundario/plantillas/febrero-res-mp3/resultado_url.php
@@ -2,10 +2,9 @@
 	<a class="Descarga" href="<?php echo $R2['dir_resultado'];?>"><?php echo $R2['dir_resultado_txt'];?></a>
 </div>
 <div class="info">
-
-<span id="repr_<?php echo $R2['num'];?>"></span>
-<script>
-AudioPlayer.embed("repr_<?php echo $R2['num'];?>",{titles:"<?php echo $R2['dir_resultado_txt'];?>",animation:"no",loader:"0x5B92C6",soundFile:"<?php echo $R2['dir_resultado'];?>"});
-</script>
+	<span id="repr_<?php echo $R2['num'];?>"></span>
+	<script>
+		AudioPlayer.embed("repr_<?php echo $R2['num'];?>",{titles:"<?php echo $R2['dir_resultado_txt'];?>",soundFile:"<?php echo $R2['dir_resultado'];?>"});
+	</script>
 	<?php echo $R2['otros_datos_mp3'];?>
 </div>
