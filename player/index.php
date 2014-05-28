b3c5f04df5bf6cba9ad0eaacf07c4f11eafc8373
Andrés Leone <forestrf@gmail.com>
2014-05-28 18:43:19 +0200

    Más css "estandarizado"

--- player/index.php
+++ player/index.php
@@ -95,20 +95,20 @@ function limpiar($que){
 						 
 						backgroundColor: "transparent",
 						backgroundGradient: "none",
-						sliderColor: '#FFFFFF',
+						sliderColor: '#FFF',
 						sliderBorder: '1.5px solid rgba(160,160,160,0.7)',
 						volumeSliderColor: '#FFFFFF',
 						volumeBorder: '1.5px solid rgba(160,160,160,0.7)',
 	
-						timeColor: '#ffffff',
+						timeColor: '#fff',
 						durationColor: '#535353',
 	
 						tooltipColor: 'rgba(255, 255, 255, 0.7)',
-						tooltipTextColor: '#000000'
+						tooltipTextColor: '#000'
 					}
 				},
 				canvas: {
-					backgroundColor:'#000000',
+					backgroundColor:'#000',
 					backgroundGradient: [0, 0]
 				}
 			});
