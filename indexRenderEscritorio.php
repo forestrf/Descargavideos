2529cd25700894673098439e6314e2993df59551
Andrés Leone <forestrf@gmail.com>
2014-05-29 19:56:59 +0200

    Aumentado el retraso de aparición de los botones sociales a 2.5s

--- indexRenderEscritorio.php
+++ indexRenderEscritorio.php
@@ -163,7 +163,7 @@ setTimeout(function(){
 	(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="//connect.facebook.net/es_ES/all.js#xfbml=1&appId=235486993147003";fjs.parentNode.insertBefore(js,fjs);}(document,'script','facebook-jssdk'));
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 	getScript("https://apis.google.com/js/plusone.js");
-},1500);
+},2500);
 
 <?php if(defined('HAY_RESULTADO')){ ?>
 	scrollTo(document.getElementById("resultado"),20);
