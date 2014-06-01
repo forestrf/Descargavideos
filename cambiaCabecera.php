3eea0753306a2be094e57957a1d738c8053ace2f
Andrés Leone <forestrf@gmail.com>
2014-06-01 18:32:57 +0200

    cambiado generador cookie estilo web

--- cambiaCabecera.php
+++ cambiaCabecera.php
@@ -4,43 +4,23 @@ include_once 'definiciones.php';
 
 //necesario la variable modo y que haya referer
 if(!isset($_GET['modo'])||!isset($_SERVER['HTTP_REFERER']))
-	fin();
-
+	exit;
 
 //necesario que el referer sea descargavideos o descargavid.blogspot
-$ref=$_SERVER['HTTP_REFERER'];
-$pag="";
+$ref=&$_SERVER['HTTP_REFERER'];
 if(
-	strpos($ref,'http://'.Dominio)==0||
-	strpos($ref,'http://www.'.Dominio)==0
+	strpos($ref,'http://'.Dominio)!=0&&
+	strpos($ref,'http://www.'.Dominio)!=0&&
+	strpos($ref,'http://descargavid.blogspot.com')!=0&&
+	strpos($ref,'http://www.descargavid.blogspot.com')!=0
 )
-$pag='descargavideos';
-
-if(
-	strpos($ref,'http://descargavid.blogspot.com')==0||
-	strpos($ref,'http://www.descargavid.blogspot.com')==0
-)
-$pag='blog_descargavideos';
-
-if($pag=="")
-	fin();
-
-$modo=$_GET['modo'];
-
-if($modo>=1 && $modo<=10){
-	$expira=time() + (60 * 60 * 24 * 30 * 12);
-	setcookie("Estilo_modo", $modo, $expira);
-}
-
-if($pag==='descargavideos')
-	fin($ref);
-else
-	fin();
-
-function fin($url="asdf"){
-	if($url=="asdf")
-		$url = 'http://www.'.Dominio.'/';
-	header('Location: '.$url);
 	exit;
+
+if($_GET['modo']>=1 && $_GET['modo']<=10){
+	setcookie('Estilo_modo', $_GET['modo'], time() + (60 * 60 * 24 * 30 * 12));
 }
+
+header('Location: /');
+
+
 ?>
