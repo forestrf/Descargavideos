a8ae099ebc15a90c423301677666a9602c6e8c57
Andrés Leone <forestrf@gmail.com>
2014-08-23 04:05:40 +0200

    sub-título de los resultados css cambiado

--- indexRenderEscritorio.php
+++ indexRenderEscritorio.php
@@ -19,9 +19,9 @@
 <?php /*? y un número es para forzar la actualización del script y del css tras cambios*/ ?>
 
 <link href="/favicon.ico" rel="icon" type="image/x-icon"/>
-<link rel="stylesheet" href="/css/cssfull.min.css?13"/>
+<link rel="stylesheet" href="/css/cssfull.min.css?14"/>
 <link rel="stylesheet" href="/css/modos/<?php echo $css_modo?>.css" id="css2"/>
-<script src="/js/funciones.min.js?5"></script>
+<script src="/js/funciones.min.js?6"></script>
 
 <script>
 var _gaq=_gaq||[];_gaq.push(["_setAccount","UA-29252510-1"]);_gaq.push(["_trackPageview"]);(function(){var ga=document.createElement("script");ga.async=true;ga.src=("https:"==document.location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(ga,s);})();
