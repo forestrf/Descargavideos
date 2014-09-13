d89e5fc97704c7d763cf838b4820a24827edd8a2
Andrés Leone <forestrf@gmail.com>
2014-09-13 16:25:50 +0200

    ampliación del commit anterior de <p> y <br>

--- idiomas/castellano/resultados.php
+++ idiomas/castellano/resultados.php
@@ -1,8 +1,8 @@
 <?php
-define('INTERIOR_AVISO_RTMP','Windows: Necesitas el programa RTMP-Downloader para descargar este vídeo.<br><a href="/lab#lab_rtmp-downloader">Descargar programa RTMP-Downloader</a>.<br>
-	<a href="http://www.youtube.com/watch?v=Jb0mt2qRXGw">video-tutorial en youtube</a>.<p>
-	Mac OS X y GNU/Linux: El programa Easy-Rtmpdump de PyDownTV te permitirá descargar este vídeo.<br><a href="http://www.pydowntv.com/ayuda#DescargarRtmpEasy">Descargar programa</a>.<br>
-	<a href="https://www.youtube.com/watch?v=Z-hsPc_Bb7A">video-tutorial en youtube</a>.');
+define('INTERIOR_AVISO_RTMP','Windows: Necesitas el programa RTMP-Downloader para descargar este vídeo.<br/><a href="/lab#lab_rtmp-downloader">Descargar programa RTMP-Downloader</a>.<br/>
+	<a href="http://www.youtube.com/watch?v=Jb0mt2qRXGw">video-tutorial en youtube</a>.<br/><br/>
+	Mac OS X y GNU/Linux: El programa Easy-Rtmpdump de PyDownTV te permitirá descargar este vídeo.<br/><a href="http://www.pydowntv.com/ayuda#DescargarRtmpEasy">Descargar programa</a>.<br/>
+	<a href="https://www.youtube.com/watch?v=Z-hsPc_Bb7A">video-tutorial en youtube</a>.<br/><br/>');
 	
-define('INTERIOR_AVISO_F4M','Windows: Necesitas el programa F4M-Downloader para descargar este vídeo.<br><a href="/lab#lab_f4m-downloader">Descargar programa F4M-Downloader</a>.<br>
-	<a href="http://www.youtube.com/watch?v=Jb0mt2qRXGw">video-tutorial en youtube</a>.<p>');
+define('INTERIOR_AVISO_F4M','Windows: Necesitas el programa F4M-Downloader para descargar este vídeo.<br/><a href="/lab#lab_f4m-downloader">Descargar programa F4M-Downloader</a>.<br/>
+	<a href="http://www.youtube.com/watch?v=Jb0mt2qRXGw">video-tutorial en youtube</a>.<br/><br/>');
