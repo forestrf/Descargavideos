afd15d9434583045df9ae0842eb45d62abdcffe8
Andrés Leone <forestrf@gmail.com>
2014-09-25 22:32:45 +0200

    xss web fix js aviso rápido

--- plantillas/extras/aviso_rapido_js.php
+++ plantillas/extras/aviso_rapido_js.php
@@ -1,12 +1,15 @@
+<?php
+	$web_escapado = strtr($R['WEB'], array('script'=>'','"'=>'\\"','\\'=>'\\\\"'));
+?>
 <span id="informaFalloRapido">Informar de resultado incorrecto</span>
 <script>
 	D.g('informaFalloRapido').onclick = function(){
-		var prmpt = prompt("Está a punto de informar que hay un problema con el resultado para el siguiente enlace:\n<?php echo strtr($web, array('script'=>'','"'=>'\\"'))?>"+"\n\nSe recomienda usar el formulario de contacto.\n\nSi lo deseas, puedes incluir un comentario junto al aviso (Si indicas tu correo electrónico podremos contestarte):\n","Insertar comentario (indique su correo si es posible)...");
+		var prmpt = prompt("Está a punto de informar que hay un problema con el resultado para el siguiente enlace:\n<?php echo $web_escapado?>\n\nSe recomienda usar el formulario de contacto.\n\nSi lo deseas, puedes incluir un comentario junto al aviso (Si indicas tu correo electrónico podremos contestarte):\n","Insertar comentario (indique su correo si es posible)...");
 		if(prmpt !== null){
 			var xmlhttp = new XMLHttpRequest();
 			xmlhttp.open("POST","/flag.php",true);
 			xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-			xmlhttp.send('url='+encodeURIComponent('<?php echo $R['WEB'];?>')+'&comentario='+encodeURIComponent(prmpt));
+			xmlhttp.send('url='+encodeURIComponent("<?php echo $web_escapado;?>")+'&comentario='+encodeURIComponent(prmpt));
 			alert('Gracias por el aviso.\nRevisaremos el fallo lo antes posible');
 		}
 	};
