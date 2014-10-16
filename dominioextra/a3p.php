bc73aaa5bc8f757ad8b0f84af6b5cac247e8637f
Andrés Leone <forestrf@gmail.com>
2014-10-16 03:55:12 +0200

    xhr + flash para a3player

--- dominioextra/a3p.php
+++ dominioextra/a3p.php
@@ -11,4 +11,80 @@ $tiempo  = $_GET['t'];
 $hmac    = $_GET['h'];
 
 ?>
-<!DOCTYPE html><html><body><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="0" height="0" id="descargador_archivos_id"><param name="movie" value="descargador_archivos.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#000" /><param name="allowScriptAccess" value="always" /><!--[if !IE]>--><embed src="descargador_archivos.swf" quality="high" bgcolor="#000" width="0" height="0" name="descargador_archivos" align="middle" play="true" loop="true" quality="high" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed><!--<![endif]--></object><script>function A(){"undefined"===typeof DESCARGADOR_ARCHIVOS_SWF?setTimeout(A,200):!0===DESCARGADOR_ARCHIVOS_SWF&&B()}function C(a){if(-1!==a.indexOf("{")){if(a=JSON.parse(a),""!=a&&"0"==a.result)return D(a.resultDes),!0}else if(""!=a&&-1!==a.indexOf("<resultDes>OK</resultDes>")&&-1!==a.indexOf("http"))return D(a.slice(a.indexOf("http")).split("<")[0].replace("&amp;","&")),!0;return!1} function B(){F("descargador_archivos").CargaWeb({url:"https://servicios.atresplayer.com/api/urlVideoLanguage/<?php echo $episode?>/android_tablet/<?php echo $episode?>|<?php echo $tiempo?>|<?php echo $hmac?>/es.json",metodo:"GET"},"procesaA3P1")}function procesaA3P1(a){C(a)||F("descargador_archivos").CargaWeb({url:"http://servicios.atresplayer.com/api/urlVideo/<?php echo $episode?>/android_tablet/<?php echo $episode?>|<?php echo $tiempo?>|<?php echo $hmac?>",metodo:"GET"},"procesaA3P2")} function procesaA3P2(a){C(a)||E()}function D(a){window.parent.postMessage('{"RESPUESTA_DV":"<?php echo $obj?>","CONTENIDO_DV":"'+a+'"}',"*")}function E(){window.parent.postMessage('{"RESPUESTA_DV":"<?php echo $obj?>","CONTENIDO_DV":false}',"*")}function F(a){return-1!=navigator.appName.indexOf("Microsoft")?window[a+"_id"]:document[a]}A();</script>
+<!DOCTYPE html>
+<html>
+<body>
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="0" height="0" id="descargador_archivos_id">
+	<param name="movie" value="descargador_archivos.swf" />
+	<param name="quality" value="high" />
+	<param name="bgcolor" value="#000" />
+	<param name="allowScriptAccess" value="always" />
+	<!--[if !IE]>-->
+	<embed src="descargador_archivos.swf" quality="high" bgcolor="#000" 
+		width="0" height="0" name="descargador_archivos" align="middle" 
+		play="true" loop="true" quality="high" allowScriptAccess="always" 
+		type="application/x-shockwave-flash" 
+		pluginspage="http://www.macromedia.com/go/getflashplayer">
+	</embed>
+	<!--<![endif]-->
+</object>
+<script>
+
+
+function preLanzaA3P(){
+	if(typeof DESCARGADOR_ARCHIVOS_SWF === "undefined"){
+		setTimeout(preLanzaA3P, 200);
+	}
+	else if(DESCARGADOR_ARCHIVOS_SWF === true){
+		lanzaA3P()
+	}
+}
+function calculaA3P(data){
+	if(data.indexOf("{") !== -1){
+		data = JSON.parse(data);
+		if(data != "" && data["result"] == "0"){
+			mostrarResultado(data["resultDes"]);
+			return true;
+		}
+	}
+	else if(data != "" && data.indexOf("<resultDes>OK</resultDes>")!==-1 && data.indexOf("http")!==-1){
+		mostrarResultado(data.slice(data.indexOf("http")).split("<")[0].replace("&amp;","&"));
+		return true;
+	}
+	return false;
+}
+function lanzaA3P(){
+	getFlashMovie("descargador_archivos").CargaWeb({
+		"url":"https://servicios.atresplayer.com/api/urlVideoLanguage/<?php echo $episode?>/android_tablet/<?php echo $episode?>|<?php echo $tiempo?>|<?php echo $hmac?>/es.json",
+		"metodo":"GET"
+	}, "procesaA3P1");
+}
+function procesaA3P1(data){
+	if(!calculaA3P(data)){
+		getFlashMovie("descargador_archivos").CargaWeb({
+			"url":"http://servicios.atresplayer.com/api/urlVideo/<?php echo $episode?>/android_tablet/<?php echo $episode?>|<?php echo $tiempo?>|<?php echo $hmac?>",
+			"metodo":"GET"
+		}, "procesaA3P2");
+	}
+}
+function procesaA3P2(data){
+	if(!calculaA3P(data)){
+		mostrarFallo();
+	}
+}
+
+function mostrarResultado(entrada){
+	window.parent.postMessage('{"RESPUESTA_DV":"<?php echo $obj?>","CONTENIDO_DV":"'+entrada+'"}','*');
+}
+
+function mostrarFallo(){
+	window.parent.postMessage('{"RESPUESTA_DV":"<?php echo $obj?>","CONTENIDO_DV":false}','*');
+}
+
+
+function getFlashMovie(m){var isIE=navigator.appName.indexOf("Microsoft")!=-1;return (isIE)?window[m+'_id']:document[m];}
+
+
+preLanzaA3P();
+
+</script>
--- dominioextra/a3p2.php
+++ dominioextra/a3p2.php
@@ -1,9 +1,9 @@
 <?php
 if(
-strpos($_SERVER['HTTP_REFERER'], 'http://www.descargavideos.tv') !== 0 &&
-strpos($_SERVER['HTTP_REFERER'], 'http://localhost/') !== 0
+	strpos($_SERVER['HTTP_REFERER'], 'http://www.descargavideos.tv') !== 0 &&
+	strpos($_SERVER['HTTP_REFERER'], 'http://localhost/') !== 0
 )
-exit;
+	exit;
 
 $obj     = $_GET['o'];
 $episode = $_GET['e'];
@@ -11,4 +11,68 @@ $tiempo  = $_GET['t'];
 $hmac    = $_GET['h'];
 
 ?>
-<!DOCTYPE html><html><body><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="0" height="0" id="descargador_archivos_id"><param name="movie" value="descargador_archivos.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#000" /><param name="allowScriptAccess" value="always" /><!--[if !IE]>--><embed src="descargador_archivos.swf" quality="high" bgcolor="#000" width="0" height="0" name="descargador_archivos" align="middle" play="true" loop="true" quality="high" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed><!--<![endif]--></object><script>function A(){if(typeof DESCARGADOR_ARCHIVOS_SWF==="undefined"){setTimeout(A,200);}else if(DESCARGADOR_ARCHIVOS_SWF===true){B()}}function C(a){if(a.indexOf("{")!==-1){a=JSON.parse(a);if(a!=""&&a.result=="0"){D(a.resultDes);return true;}}return false;}function B(){G("descargador_archivos").CargaWeb({"url":"https://servicios.atresplayer.com/api/urlVideoLanguage/<?php echo $episode?>/web/<?php echo $episode?>|<?php echo $tiempo?>|<?php echo $hmac?>/es.json","metodo":"GET"},"E");}function E(a){if(!C(a)){F();}}function D(a){window.parent.postMessage('{"RESPUESTA_DV":"<?php echo $obj?>","CONTENIDO_DV":"'+a+'"}','*');}function F(){window.parent.postMessage('{"RESPUESTA_DV":"<?php echo $obj?>","CONTENIDO_DV":false}','*');}function G(m){var a=navigator.appName.indexOf("Microsoft")!=-1;return (a)?window[m+'_id']:document[m];}A();</script>
+<!DOCTYPE html>
+<html>
+<body>
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="0" height="0" id="descargador_archivos_id">
+	<param name="movie" value="descargador_archivos.swf" />
+	<param name="quality" value="high" />
+	<param name="bgcolor" value="#000" />
+	<param name="allowScriptAccess" value="always" />
+	<!--[if !IE]>-->
+	<embed src="descargador_archivos.swf" quality="high" bgcolor="#000" 
+		width="0" height="0" name="descargador_archivos" align="middle" 
+		play="true" loop="true" quality="high" allowScriptAccess="always" 
+		type="application/x-shockwave-flash" 
+		pluginspage="http://www.macromedia.com/go/getflashplayer">
+	</embed>
+	<!--<![endif]-->
+</object>
+<script>
+
+
+function preLanzaA3P(){
+	if(typeof DESCARGADOR_ARCHIVOS_SWF === "undefined"){
+		setTimeout(preLanzaA3P, 200);
+	}
+	else if(DESCARGADOR_ARCHIVOS_SWF === true){
+		lanzaA3P()
+	}
+}
+function calculaA3P(data){
+	if(data.indexOf("{") !== -1){
+		data = JSON.parse(data);
+		if(data != "" && data["result"] == "0"){
+			mostrarResultado(data["resultDes"]);
+			return true;
+		}
+	}
+	return false;
+}
+function lanzaA3P(){
+	getFlashMovie("descargador_archivos").CargaWeb({
+		"url":"https://servicios.atresplayer.com/api/urlVideoLanguage/<?php echo $episode?>/web/<?php echo $episode?>|<?php echo $tiempo?>|<?php echo $hmac?>/es.json",
+		"metodo":"GET"
+	}, "procesaA3P2");
+}
+function procesaA3P2(data){
+	if(!calculaA3P(data)){
+		mostrarFallo();
+	}
+}
+
+function mostrarResultado(entrada){
+	window.parent.postMessage('{"RESPUESTA_DV":"<?php echo $obj?>","CONTENIDO_DV":"'+entrada+'"}','*');
+}
+
+function mostrarFallo(){
+	window.parent.postMessage('{"RESPUESTA_DV":"<?php echo $obj?>","CONTENIDO_DV":false}','*');
+}
+
+
+function getFlashMovie(m){var isIE=navigator.appName.indexOf("Microsoft")!=-1;return (isIE)?window[m+'_id']:document[m];}
+
+
+preLanzaA3P();
+
+</script>
