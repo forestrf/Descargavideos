5f2a429748e82c565cf70f51780d96a86cbc4cf1
Andrés Leone <forestrf@gmail.com>
2015-02-05 21:05:57 +0100

    Agregado botón donar tmbn a la página principal.

--- paginas/pag_principal.php
+++ paginas/pag_principal.php
@@ -62,5 +62,12 @@
 	</div>
 	<div class="bloque_pagina bloqueMargin txt_centrado">
 		<?php echo TXT_BLOQUE_DONACION?>
+		<br/><br/>
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="TWBMG8NXCKZJN">
+			<input type="image" src="https://www.paypalobjects.com/es_ES/ES/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal. La forma rápida y segura de pagar en Internet.">
+			<img alt="" border="0" src="https://www.paypalobjects.com/es_ES/i/scr/pixel.gif" width="1" height="1">
+		</form>
 	</div>
 </div>
