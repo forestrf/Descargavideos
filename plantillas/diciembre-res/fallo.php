769811c047c65856c7169e192e0081ded4608f76
Andrés Leone <forestrf@gmail.com>
2015-06-18 20:44:41 +0200

    definiendo 'IGNORA_AVISO_RAPIDO' se puede quitar el botón para informar de un problema en un resultado

--- plantillas/diciembre-res/fallo.php
+++ plantillas/diciembre-res/fallo.php
@@ -1,5 +1,7 @@
 <div class="error_res"><?php echo $R['error_texto'];?></div>
 
-<?php if($R['WEB'] !== '') include 'plantillas/extras/aviso_rapido_js.php'?>
+<?php
+if($R['WEB'] !== '' && !defined('IGNORA_AVISO_RAPIDO')) include 'plantillas/extras/aviso_rapido_js.php'
+?>
 
 <script>_gaq.push(["_trackEvent","Descargas","Fallidas","Versión sin info extra"]);</script>
