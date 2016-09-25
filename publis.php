fd9b9adf59e2ec0b9dad6b13c6f65be42585f9a8
Andrés Leone <forestrf@gmail.com>
2016-09-25 02:50:44 +0200

    add extra publi

--- publis.php
+++ publis.php
@@ -1,4 +1,6 @@
 <?php
+$publisUsadas = array();
+
 /*
 $publis=array(
 	'300x250' => '<!-- BEGIN JS TAG - [Descargavideos] - Default < - DO NOT MODIFY -->
@@ -15,8 +17,6 @@ $publis=array(
 
 );
 
-$publisUsadas = array();
-
 function getPubli($i){
 	global $publis, $publisUsadas, $PUBLICIDAD;
 	//return '';
@@ -29,11 +29,19 @@ function getPubli($i){
 }
 */
 function getPubliJS($width, $height){
+	global $publisUsadas;
+	if (isset($publisUsadas[$width.'x'.$height]))
+		return '';
+	$publisUsadas[$width.'x'.$height] = true;
 	return '<!-- BEGIN JS TAG - [Descargavideos] - Default < - DO NOT MODIFY -->
 		<SCRIPT SRC="http://ib.adnxs.com/ttj?id=8551763&size='.$width.'x'.$height.'" TYPE="text/javascript"></SCRIPT>
 		<!-- END TAG -->';
 }
 function getPubliIFRAME($width, $height){
+	global $publisUsadas;
+	if (isset($publisUsadas[$width.'x'.$height]))
+		return '';
+	$publisUsadas[$width.'x'.$height] = true;
 	return '<!-- BEGIN JS TAG - [Descargavideos] - Default < - DO NOT MODIFY -->
 		<IFRAME SRC="http://ib.adnxs.com/tt?id=8551763&size='.$width.'x'.$height.'" FRAMEBORDER="0" SCROLLING="no" MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" LEFTMARGIN="0" ALLOWTRANSPARENCY="true" WIDTH="'.$width.'" HEIGHT="'.$height.'"></IFRAME>
 		<!-- END TAG -->';
