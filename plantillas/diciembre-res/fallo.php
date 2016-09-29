e3d5739cee993f75f686098a6e5189bd01827fad
Andrés Leone <forestrf@gmail.com>
2016-09-29 14:27:19 +0200

    cambiada posición de la publi en error

--- plantillas/diciembre-res/fallo.php
+++ plantillas/diciembre-res/fallo.php
@@ -1,10 +1,10 @@
 <div class="error_res"><?php echo $R['error_texto'];?></div>
 
-<center><?php echo getPubliJS(300, 250, true)?></center>
-
 <?php
 if($R['WEB'] !== '' && !(defined('IGNORA_AVISO_RAPIDO') || !IGNORA_AVISO_RAPIDO)) include 'plantillas/extras/aviso_rapido_js.php';
 $error_tracker_msg = defined('ERROR_TRACK_NAME') ? json_encode(ERROR_TRACK_NAME) : '"Versión sin info extra"';
 ?>
+
+<center><?php echo getPubliJS(300, 250, true)?></center>
 
 <script>_gaq.push(["_trackEvent","Descargas","Fallidas",<?php echo $error_tracker_msg?>]);</script>
