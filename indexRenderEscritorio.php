462dd7f48c050c9acf1bfc38b615e4bcfafb0884
Andrés Leone <forestrf@gmail.com>
2016-10-09 17:27:42 +0200

    Quitado cache.

--- indexRenderEscritorio.php
+++ indexRenderEscritorio.php
@@ -4,6 +4,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta http-equiv="pragma" content="no-cache"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
 
 <title><?php echo TXT_TITLE?> - DescargaVideos.TV<?php echo $seccion==""?"":" - ".$seccion?></title>
