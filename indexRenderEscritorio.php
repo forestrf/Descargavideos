70842e36cf00ad7d9c1a5f90357e561190bf0e95
Andrés Leone <forestrf@gmail.com>
2016-10-10 02:20:38 +0200

    anti-cache meta-tag updated

--- indexRenderEscritorio.php
+++ indexRenderEscritorio.php
@@ -4,7 +4,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-<meta http-equiv="cache-control" content="no-cache"/>
+<meta http-equiv="cache-control" content="no-cache, must-revalidate, post-check=0, pre-check=0"/>
 <meta http-equiv="expires" content="0"/>
 <meta http-equiv="pragma" content="no-cache"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"/>
