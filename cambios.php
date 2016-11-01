f4e45810760d0c7c1e95c7ea0f7fb69e37f51ab3
Andrés Leone <forestrf@gmail.com>
2016-11-01 05:25:39 +0100

    transcripciones rtve (anotado en cambios)

--- cambios.php
+++ cambios.php
@@ -17,6 +17,7 @@ function cambios($max=-1){
 
 $todos[]=array(
 '1-Noviembre-2016'
+,'add:Extractor de transcripciones de subtítulos de RTVE a srt.'
 ,'fix:Discovery Max vuelve a mostrar una miniatura del vídeo en el resultado.'
 ,'fix:8tv.cat vuelve a funcionar. Ahora lista varias calidades y por descarga directa.'
 ,'fix:facebook vuelve a funcionar.'
