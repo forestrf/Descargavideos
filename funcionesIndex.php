53ab499ce48f26bf8c08daa5dd32699d57320ef3
Andrés Leone <forestrf@gmail.com>
2016-11-04 16:41:57 +0100

    soporte https para publi

--- funcionesIndex.php
+++ funcionesIndex.php
@@ -118,4 +118,10 @@ function preparaPagina($cual='pag_principal'){
 		break;
 	}
 }
+
+function isSecure() {
+	return
+		(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+		|| $_SERVER['SERVER_PORT'] == 443;
+}
 ?>
--- publis.php
+++ publis.php
@@ -7,7 +7,7 @@ function getPubliJS($width, $height, $ignorePublisUsadas = false){
 		return '';
 	$publisUsadas[$width.'x'.$height] = true;
 	return '<!-- BEGIN JS TAG - [Descargavideos] - Default < - DO NOT MODIFY -->
-		<SCRIPT SRC="http://ib.adnxs.com/ttj?id=8551763&size='.$width.'x'.$height.'" TYPE="text/javascript"></SCRIPT>
+		<SCRIPT SRC="//ib.adnxs.com/ttj?id=8551763&size='.$width.'x'.$height.'" TYPE="text/javascript"></SCRIPT>
 		<!-- END TAG -->';
 }
 function getPubliIFRAME($width, $height, $ignorePublisUsadas = false, $style = ''){
@@ -16,6 +16,6 @@ function getPubliIFRAME($width, $height, $ignorePublisUsadas = false, $style = '
 		return '';
 	$publisUsadas[$width.'x'.$height] = true;
 	return '<!-- BEGIN JS TAG - [Descargavideos] - Default < - DO NOT MODIFY -->
-		<IFRAME SRC="http://ib.adnxs.com/tt?id=8551763&size='.$width.'x'.$height.'" FRAMEBORDER="0" SCROLLING="no" MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" LEFTMARGIN="0" ALLOWTRANSPARENCY="true" WIDTH="'.$width.'" HEIGHT="'.$height.'" style="'.$style.'"></IFRAME>
+		<IFRAME SRC="//ib.adnxs.com/tt?id=8551763&size='.$width.'x'.$height.'" FRAMEBORDER="0" SCROLLING="no" MARGINHEIGHT="0" MARGINWIDTH="0" TOPMARGIN="0" LEFTMARGIN="0" ALLOWTRANSPARENCY="true" WIDTH="'.$width.'" HEIGHT="'.$height.'" style="'.$style.'"></IFRAME>
 		<!-- END TAG -->';
 }
