6b9a777d59aa1caa2ac112ed0a6782250f72f9f7
Andrés Leone <forestrf@gmail.com>
2017-01-30 16:30:06 +0100

    fix rtve quitada opción flv porque a veces no va, pero mp4 siempre va

--- secundario/rtve.php
+++ secundario/rtve.php
@@ -249,7 +249,7 @@ function convierteID($asset,$modo=array('video','audio')){
 		$ret = self::desencripta($ret);
 		
 		dbug_($ret);
-		if(preg_match_all('@http://[^<^>]*?\\.(?:mp4|flv|mp3)[^<^>]*@',$ret, $m)){
+		if(preg_match_all('@http://[^<^>]*?\\.(?:mp4|mp3)[^<^>]*@',$ret, $m)){
 			dbug_r($m);
 			$it = 1;
 			foreach($m[0] as $i){
