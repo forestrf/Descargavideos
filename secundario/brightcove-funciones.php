83582b7ccaadb550becf38356e2be3ee5941d4bd
Andrés Leone <forestrf@gmail.com>
2017-02-09 02:35:04 +0100

    fix regresión brightcove

--- secundario/brightcove-funciones.php
+++ secundario/brightcove-funciones.php
@@ -122,7 +122,7 @@ function URLSDelArrayBrightCove($r, $tipo, &$obtenido_enlaces, $titulo){
 }
 
 function BrightCove_Api($web_descargada, &$obtenido) {
-	if (preg_match('@(//players.brightcove.net/([0-9]+?)/(?:.+videoId=([0-9]+)|.+).+)"@', $web_descargada, $matches)) {
+	if (preg_match('@(//players.brightcove.net/([0-9]+)/(?:.*videoId=([0-9]+))?.*)"@', $web_descargada, $matches)) {
 		dbug_r($matches);
 		/* Para que la url de la api funcione hay que enviar la cabecera
 		Accept: application/json;pk=BCpkADawqM0yNpRcqDSHVNbUNbcN95zKSM60CvPyMAaTR-Gr8cF3B6l2mV8foou1rmu08m3an1yjj-ikUuwKjVMLSKEolTur7xeCJlf2QrnaAodMX2l4WfINughBDczNizUI6su9QedX7xJ-
