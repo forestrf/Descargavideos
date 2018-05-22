a9a5218c494abdeefbdb67259fc97d57f85b8e3f
Andrés <forestrf@gmail.com>
2018-05-22 23:48:49 +0200

    Privacidad actualizada para Adversal

--- paginas/privacidad.php
+++ paginas/privacidad.php
@@ -1,1 +1,5 @@
 <?php echo TXT_PRIVACIDAD_FULL?>
+<br/>
+<br/>
+Adversal Media, Inc.<br>
+(<a href='https://www.adversal.com/online-privacy.html' target='_blank'>https://www.adversal.com/online-privacy.html</a>)
