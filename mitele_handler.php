46d46e3c3bfe7c8e7bfd5ea35a69504672f4faff
Andrés Leone <forestrf@gmail.com>
2022-06-22 00:58:55 +0200

    misc

--- mitele_handler.php
+++ mitele_handler.php
@@ -2,5 +2,8 @@
 include_once 'definiciones.php';
 include_once 'funciones.php';
 
+echo "nope";
+exit;
+
 header("HTTP/1.1 302 Moved Temporarily");
 header('Location: http://adf.ly/3649838/'.urlencode('http://www.descargavideos.tv'.$_SERVER['REQUEST_URI']));
